f90476d128f6e14a561b122e5f4baa0a0665d468
User21 <user21@user21de-imac.local>
2018-06-27 08:08:10 +0800

    7777777

--- 00457021/ViewController.swift
+++ 00457021/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imgImageView.loadGif(name: "kitty和熊熊")
     }
 
     override func didReceiveMemoryWarning() {
